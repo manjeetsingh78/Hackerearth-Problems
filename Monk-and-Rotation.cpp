241534e6ebcda86d6f83a625841b9be421ddd741
Manjeet Singh <rambaghel741791@gmail.com>
2026-07-19 00:17:56 +0530

    Monk and Rotation problem has been solved

---
+++ Monk-and-Rotation.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int num;
+	cin >> num;
+	for(int i = 0;i<num;i++){
+		int a,b;
+		cin>>a>>b;
+		vector<int>arr;
+		for(int j = 0;j<a;j++){
+			int n;
+			cin>>n;
+			arr.push_back(n);
+		}
+		
+		for(int j = a-b; j<a;j++){
+			cout<<arr[j]<<" ";
+		}
+		for(int j = 0;j<a-b;j++){
+			cout<<arr[j]<<" ";
+		}
+		cout<< endl;
+	}
+}
